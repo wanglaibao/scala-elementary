48b5d9860ff6540e68aec5a9b9e631c11d665e25
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-09-16 15:54:36 +0800

    update scala files info

--- src/main/scala/com/laibao/chapter0/Greeting.scala
+++ src/main/scala/com/laibao/chapter0/Greeting.scala
@@ -6,7 +6,9 @@ package com.laibao.chapter0
   * @version 1.0
   */
 object Greeting extends App{
-
+  /**
+    * 字符串表达式： s"...${expression}..."
+    */
   for (i <- 0 to 3) {
     print(s"$i ,")
   }
