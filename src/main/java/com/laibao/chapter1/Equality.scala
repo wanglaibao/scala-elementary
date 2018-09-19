15cf78764d4e0acbf35d2baf52337d341680aa5b
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-09-19 11:06:45 +0800

    update scala files info

---
+++ src/main/java/com/laibao/chapter1/Equality.scala
@@ -0,0 +1,20 @@
+package com.laibao.chapter1
+
+object Equality {
+
+  def main(args: Array[String]): Unit = {
+      val str1 = "hello,world"
+
+      val str2 = "hello,world"
+
+      val str3 = new String("hello,world")
+
+      println(str1 == str2)
+
+      println(str1 == str3)
+
+      println(str1.eq(str2))
+
+      println(str1.eq(str3))
+  }
+}
