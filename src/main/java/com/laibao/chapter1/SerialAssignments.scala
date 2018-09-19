01c152cd57707c2ea0ff0a6eb78730ac77fc0b49
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-09-19 10:59:25 +0800

    update scala files info

---
+++ src/main/java/com/laibao/chapter1/SerialAssignments.scala
@@ -0,0 +1,11 @@
+package com.laibao.chapter1
+
+object SerialAssignments {
+
+  var a = 1
+  var b = 2
+
+  //Expression to type Unit dosen't conform to expected type Int
+  a = b = 3
+
+}
--- src/main/scala/com/laibao/chapter0/Greeting.scala
+++ src/main/scala/com/laibao/chapter0/Greeting.scala
@@ -9,8 +9,14 @@ object Greeting extends App{
   /**
     * 字符串表达式： s"...${expression}..."
     */
-  for (i <- 0 to 3) {
+  for (i <- 1 to 4) {
     print(s"$i ,")
   }
   println("Scala Rocks")
+
+  for (j <- 1 until  100) {
+    print(s"$j ,")
+  }
+  println()
+
 }
